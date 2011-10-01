3a9c20805b85374a0dc8e83489db6a27f86fc3cd
Ilnur Khalilov <xomyaq@gmail.com>
2011-10-02 05:19:43 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "cone.h"
+
+int main() {
+	double conicalIndenterWeight = 100;
+	double conicalIndenterInitialSpeed = 100;
+	double solidHalfSpaceDensity = 100;
+	double solidHalfSpaceCoefficient = 100;
+	double solidHalfSpaceFriction = 100; 
+	double solidHalfSpaceStrength = 100; 
+
+	Cone cone(solidHalfSpaceDensity,
+		solidHalfSpaceCoefficient,
+		solidHalfSpaceFriction,
+		solidHalfSpaceStrength,
+		conicalIndenterWeight,
+		conicalIndenterInitialSpeed);
+	
+	return 0;
+}
