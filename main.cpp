bf3b0f6671e0f122a3fd0a4475b614c422752f4a
Ilnur Khalilov <xomyaq@gmail.com>
2011-10-09 17:16:35 +0600

    Remove header files and rewrite code with no classes

--- main.cpp
+++ main.cpp
@@ -1,20 +1,59 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <iostream>
-#include "spaceIndenter.h"
 
-int main() {
+double density0;
+double strength0;
+double friction0;
+double mass;
+double inititalSpeed;
+
+double density(double z) {
+	return density0;
+}
+
+double strength(double z) {
+	return strength0;
+}
+
+double friction(double z) {
+	return friction0;
+}
+
+double f(double z) {
+	return z;
+}
+
+double df(double z) {
+	double h = 0.0001;
+
+	return (f(z + h) - f(z - h)) / (2 * h);
+}
+
+double k(double z) {
+	return pow(sin(atan(df(z))), 2);
+}
+
+double F1(double x) {
+	return 1;
+}
+
+double F2(double x) {
+	return 1;
+}
+
+int main(int argc, char **argv) {
 	double conicalIndenterWeight = 0.001 / 981;
 	double conicalIndenterInitialSpeed = 30000;
 	double solidHalfSpaceDensity = 0.0027 / 981;
 	double solidHalfSpaceFriction = 200; 
-	double solidHalfSpaceStrength = 1000; 
+	double solidHalfSpaceStrength = 1000;
 
-	SpaceIndenter cone(solidHalfSpaceDensity,
-		solidHalfSpaceFriction,
-		solidHalfSpaceStrength,
-		conicalIndenterWeight,
-		conicalIndenterInitialSpeed);
-
-	std::cout << cone.solve(0.2);
+	density0 = solidHalfSpaceDensity;
+	strength0 = solidHalfSpaceStrength;
+	friction0 = solidHalfSpaceFriction;
+	mass = conicalIndenterWeight;
+	inititalSpeed = conicalIndenterInitialSpeed;
 	
 	std::cin.get();
 
